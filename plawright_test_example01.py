c0532624531677dcf20cf1c8094ac63cccd14945
Nemanja Djurovic <nemanja.djurovic@htecgroup.com>
2024-03-12 13:32:03 +0100

    just changes

--- plawright_test_example01.py
+++ plawright_test_example01.py
@@ -2,11 +2,12 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False, slow_mo=300)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
     page.wait_for_load_state("networkidle")
+    expect(page.get_by_role("button", name="Log In")).to_be_visible()
     page.get_by_role("button", name="Log In").click()
     page.get_by_test_id("signUp.switchToSignUp").click()
     page.get_by_role("button", name="Log in with Email").click()
@@ -14,6 +15,8 @@ def run(playwright: Playwright) -> None:
     page.get_by_label("Password").click()
     page.get_by_label("Password").fill("test123")
     page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
+    page.wait_for_load_state("networkidle")
+    expect(page.get_by_role("button", name="Log In")).to_be_hidden()
     print("Wohooo!")
 
     # ---------------------
