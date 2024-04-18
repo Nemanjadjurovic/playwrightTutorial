b2d2416d7769122ed0266bc62f3b0d4d06267cf3
Nemanja Djurovic <nemanja.djurovic@htecgroup.com>
2024-04-18 14:09:25 +0200

    Idemooo novo

---
+++ tests_ui_layout/test_contact_us_form.py
@@ -0,0 +1,17 @@
+import time
+
+from playwright.sync_api import Playwright, sync_playwright
+# import pytest
+from pom.contact_us_page import ContactUsPage
+
+
+def test_submit_form(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    contact_us = ContactUsPage(page)
+    contact_us.navigate()
+    contact_us.submit_form("Nemanja", "ulica 123", "email@email.com", "123-321-333", "test subject", "poruka ide")
+
+
+with sync_playwright() as playwright:
+    test_submit_form(playwright)
