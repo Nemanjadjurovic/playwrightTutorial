ff6bb996806bb663aa7270800a6f5110a5582cd2
Nemanja Djurovic <nemanja.djurovic@htecgroup.com>
2024-04-18 13:06:23 +0200

    pom ide

--- tests_ui_layout/plawright_test_example01.py
+++ tests_ui_layout/plawright_test_example01.py
@@ -1,11 +1,14 @@
+import time
+
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+    browser = playwright.chromium.launch(headless=False)  #slow_mo=500
     context = browser.new_context()
     page = context.new_page()
     page.set_default_timeout(5000)
+    time.sleep(2)
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
     page.wait_for_load_state("networkidle")
     expect(page.get_by_role("button", name="Log In")).to_be_visible()
