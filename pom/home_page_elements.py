2307dd7ec550e791caa774e342452560be41d986
Nemanja Djurovic <nemanja.djurovic@htecgroup.com>
2024-04-25 13:43:05 +0200

    Cetvrtak

--- pom/home_page_elements.py
+++ pom/home_page_elements.py
@@ -1,7 +1,6 @@
 
 
 class HomePage:
-
     def __init__(self, page):
         self.celebrate_header = page.get_by_text("Celebrating Beauty and Style")
         self.celebrate_body = page.get_by_text("playwright-practice was founded by a group of"
--- tests_ui_layout/test_login.py
+++ tests_ui_layout/test_login.py
@@ -3,20 +3,24 @@ import pytest
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
+@pytest.mark.parametrize("email, password", [("symon.storozhenko@gmail.com", "test123"),
+                                        pytest.param("fakeemail", "fakepassword", marks=pytest.mark.xfail),
+                                        pytest.param("symon.storozhenko@gmail", "test123", marks=pytest.mark.xfail)])
 @pytest.mark.smoke
-def test_login(set_up) -> None:
+def test_login(set_up, email, password) -> None:
     page = set_up
     page.wait_for_load_state("networkidle")
     expect(page.get_by_role("button", name="Log In")).to_be_visible()
     page.get_by_role("button", name="Log In").click()
     page.get_by_test_id("signUp.switchToSignUp").click()
     page.get_by_role("button", name="Log in with Email").click()
-    page.get_by_test_id("emailAuth").get_by_label("Email").fill("symon.storozhenko@gmail.com")
+    page.get_by_test_id("emailAuth").get_by_label("Email").fill(email)
     page.get_by_label("Password").click()
-    page.get_by_label("Password").fill("test123")
+    page.get_by_label("Password").fill(password)
     page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
     # page.wait_for_load_state("networkidle")
     # expect(page.get_by_role("button", name="Log In")).to_be_hidden()
+    expect(page.get_by_text("Welcome")).to_be_visible()
 
     all_links = page.get_by_role("link").all()
     for link in all_links:
