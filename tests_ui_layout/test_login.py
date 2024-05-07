633c2fdbbaa743a167fc07ae52016b1037a8dc87
Nemanja Djurovic <nemanja.djurovic@htecgroup.com>
2024-05-07 10:34:17 +0200

    adding env file to the framework

--- tests_ui_layout/test_login.py
+++ tests_ui_layout/test_login.py
@@ -4,12 +4,13 @@ import time
 import pytest
 from playwright.sync_api import Playwright, sync_playwright, expect
 
+PASSWORD = os.environ['PASSWORD']
 
-try:
-    PASSWORD = os.environ['PASSWORD']
-except KeyError:
-    import utils.secret_config
-    PASSWORD = utils.secret_config.PASSWORD
+# try:
+#     PASSWORD = os.environ['PASSWORD']
+# except KeyError:
+#     import utils.secret_config
+#     PASSWORD = utils.secret_config.PASSWORD
 
 
 @pytest.mark.parametrize("email, password", [("symon.storozhenko@gmail.com", PASSWORD), #("symon.storozhenko@gmail.com", utils.secret_config.PASSWORD)
