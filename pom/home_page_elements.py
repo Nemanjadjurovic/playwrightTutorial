8cc8ade3157968e3c11f664af3e7ad149e5c9482
Nemanja Djurovic <nemanja.djurovic@htecgroup.com>
2024-05-14 09:22:55 +0200

    visual testing done

--- pom/home_page_elements.py
+++ pom/home_page_elements.py
@@ -5,3 +5,4 @@ class HomePage:
         self.celebrate_header = page.get_by_text("Celebrating Beauty and Style")
         self.celebrate_body = page.get_by_text("playwright-practice was founded by a group of"
                                                " like-minded fashion devotees, dete")
+        self.celebrating_beauty_hdr = page.get_by_label("Shoes gallery").locator("a").filter(has_text="Quick View")
---
+++ tests_ui_layout/test_landing_page_visual.py
@@ -0,0 +1,15 @@
+from pom.home_page_elements import HomePage
+from playwright.sync_api import Playwright, sync_playwright, expect
+import pytest
+
+
+def test_visual_landing(page, assert_snapshot):
+    url = "https://symonstorozhenko.wixsite.com/website-1/shop"
+    page.goto(url)
+    homepage = HomePage(page)
+    expect(homepage.celebrating_beauty_hdr).to_be_visible()
+    assert_snapshot(page.screenshot())
+    # expect(page).to_have_url(url)
+    # url_name = expect(page).to_have_url(f"{url}")
+    # print(url_name)
+    # print(url)
